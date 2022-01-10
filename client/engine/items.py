a55b10aecc37edc1991181cc3948c52c59604e9c
wittoku <97328301+wittoku@users.noreply.github.com>
2022-01-10 13:48:32 -0500

    check for item in inventory before allowing use

--- client/engine/items.py
+++ client/engine/items.py
@@ -16,44 +16,47 @@ class PokePermItem:
         """
         evolution_manager: EvolutionManager = self.state.evolution_manager
         pokemon_factory: PokemonFactory = self.state.pokemon_factory
+        if self.name in player.inventory.keys():
+            if player.inventory[self.name] > 0:
+                """
+                TM
+                """
+                if self.name == 'TM':
+                    if pokemon.battle_card.tm_flag == 1:
+                        print('It had no effect')
+                    else:
+                        pokemon.battle_card.tm_flag = 1
+            
+                        player.remove_item(self.name)
+                """
+                Rare Candy
+                """
+                if self.name == 'Rare Candy':
+                    if evolution_manager.get_threshold( pokemon.name) != None:
+                        pokemon.xp += 50
+                        player.remove_item(self.name)
+                        evolution_manager.evolution_checker(player, pokemon)
+                        pokemon_factory.shiny_checker(player, pokemon)
+                    else:
+                        print('It had no effect')
+                """
+                Rental Ditto
+                """
+                if self.name == 'Rental Ditto':
+                    
+                    clone = pokemon_factory.create_pokemon_by_name(pokemon.name)
+                    if player.add_to_roster(clone) == True:
+                        pokemon_factory.shiny_checker(player, pokemon)
+                        player.remove_item(self.name)
+                    else:
+                        print('It had no effect')
 
-        if player.inventory[self.name] > 0:
-            """
-            TM
-            """
-            if self.name == 'TM':
-                if pokemon.battle_card.tm_flag == 1:
-                    print('It had no effect')
-                else:
-                    pokemon.battle_card.tm_flag = 1
         
-                    player.remove_item(self.name)
-            """
-            Rare Candy
-            """
-            if self.name == 'Rare Candy':
-                if evolution_manager.get_threshold( pokemon.name) != None:
-                    pokemon.xp += 50
-                    player.remove_item(self.name)
-                    evolution_manager.evolution_checker(player, pokemon)
-                    pokemon_factory.shiny_checker(player, pokemon.name)
-                else:
-                    print('It had no effect')
-            """
-            Rental Ditto
-            """
-            if self.name == 'Rental Ditto':
-                
-                clone = pokemon_factory.create_pokemon_by_name(pokemon.name)
-                if player.add_to_roster(clone) == True:
-                    pokemon_factory.shiny_checker(player, pokemon.name)
-                    player.remove_item(self.name)
-                else:
-                    print('It had no effect')
-
-        
+            else:
+                print('None present in inventory')
         else:
-            print('None present in inventory')
+                print('None present in inventory')
+
 
 
 
@@ -67,7 +70,7 @@ class _Item:
     #leaf stone: if type1 == grass or type2 == grass: XP+3
     #thunder stone: if type1 == electric or type2 == electric: XP+3
     #eviolite: def buff if NFE
-    #choice specs: your fast move becomes lock on. +2 attack buff
+    #choice specs: your fast move becomes lock on (or maybe yawn?). +2 attack buff
     #choice band: your charged move(s) become frustration. +2 attack buff
     #weakness policy: if opponent has super effective move, +1 attack buff
 
@@ -75,10 +78,10 @@ class _Item:
     #master ball: next buy is free
     #poke flute: next N rerolls are free
 
-    #battle items: use and consume
+    #battle items: one round only
     #ganlon berry: an extra shield
-    #oran berry: if poke survives a fight, restore 10hp
-    #sitrus berry: if poke survives a fight, restore 30 hp
+    #oran berry: if poke survives a fight, restore 10%hp
+    #sitrus berry: if poke survives a fight, restore 30%hp
     #leppa berry: start with 10 energy
     #power herb: start with 30 energy
     #salac berry: if poke survives, gain 40 energy
