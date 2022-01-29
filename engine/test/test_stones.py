bd1c59ea48d4527455215346502e9ce8b5c4505e
wittoku <97328301+wittoku@users.noreply.github.com>
2022-01-29 11:50:14 -0500

    Create test_stones.py

---
+++ engine/test/test_stones.py
@@ -0,0 +1,30 @@
+from engine.env import Environment
+from engine.items import ItemManager
+from engine.models.player import Player
+from engine.pokemon import EvolutionManager, PokemonFactory
+from engine.models.items import PersistentPokemonItem
+from engine.models.items import PersistentPlayerItem
+from engine.models.items import InstantPokemonItem
+from engine.models.items import InstantPlayerItem
+from engine.models.pokemon import Pokemon
+from engine.player import PlayerManager
+from engine.models.association import PlayerInventory
+env = TestEnvironment(8)
+env = Environment.create_webless_game(4)
+env.initialize()
+player = Player(name='balbert bang')
+poke_factory: PokemonFactory = env.pokemon_factory      
+player_manager: PlayerManager = env.player_manager
+player_manager: PlayerManager = env.player_manager
+player_manager.create_and_give_pokemon_to_player(player, 'squirtle') 
+player_manager.create_and_give_item_to_player(player, 'WaterStone')
+squirtle = player_manager.player_roster(player)[0] 
+water_stone = PlayerInventory.get_inventory(player)[0] 
+player_manager.give_item_to_pokemon(squirtle, water_stone)
+water_stone.use()
+player_manager.remove_item_from_pokemon(squirtle)
+player_manager.create_and_give_item_to_player(player, 'WaterStone')
+water_stone = PlayerInventory.get_inventory(player)[0] 
+player_manager.give_item_to_pokemon(squirtle, water_stone)
+water_stone.use()
+
