fb4f399942ebb14ccc277c91c7ccc0b885fee5fc
wittoku <97328301+wittoku@users.noreply.github.com>
2022-02-03 13:27:10 -0500

    custom buff rendering

--- replay-embed-fast.js
+++ replay-embed-fast.js
@@ -48,7 +48,7 @@ requireScript('https://play.pokemonshowdown.com/data/abilities.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
 requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
+requireScript('render_battle.js');
 
 var Replays = {
 	battle: null,
