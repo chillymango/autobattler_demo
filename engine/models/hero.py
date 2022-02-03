421973f17d836d551ed7e9be02a6e973f72de2e1
wittoku <97328301+wittoku@users.noreply.github.com>
2022-02-02 21:31:21 -0500

    hero showdown avatar

--- engine/models/hero.py
+++ engine/models/hero.py
@@ -27,6 +27,7 @@ class Hero(BaseModel):
     ability_name: str
     _power: T.Type[HeroPowerType] = PrivateAttr(default=None)
     _env: "Environment" = PrivateAttr(default=None)
+    showdown_avatar: str
 
     def __init__(self, _env=None, _power=None, **kwargs):
         super().__init__(**kwargs)
@@ -49,22 +50,22 @@ class Hero(BaseModel):
 
 
 NORMAL_HEROES: T.List[Hero] = [
-    Hero(name="Brock", ability_name="Brock Solid", _power=BrockShieldPower),
-    Hero(name="Blaine", ability_name="PUSH THE BUTTON", _power=BlaineButton),
-    Hero(name="Blue", ability_name = "Smell 'ya Later", _power=BlueSmell),
-    Hero(name="Bruno", ability_name="Thicc Bod", _power=BrunoBod),
-    Hero(name="Erika", ability_name="Herb Master", _power=ErikaGarden),
-    Hero(name="Giovanni", ability_name="Crypto Dividends", _power=GiovanniGains),
-    Hero(name='Green', ability_name='Mineral Collector', _power = GreensRocks),
-    Hero(name='Janine', ability_name='Ninjutsu Art of Substitution', _power = KogaNinja),
-    Hero(name='Koga', ability_name='Ninjutsu Art of Summoning', _power = JanineJutsu),
-    Hero(name='Lance', ability_name='"Dragon" Master', _power = LanceFetish),
-    Hero(name='Lt. Surge', ability_name='Gorilla Warfare', _power = SurgeGorilla),
-    Hero(name='Misty', ability_name='Trust Fund', _power = MistyTrustFund),
-    Hero(name='Red', ability_name='Plot Armor', _power = RedCheater),
-    Hero(name='Jessie and James', ability_name='Blast Off!!', _power = BlastOff),
-    Hero(name='Sabrina', ability_name='Clairvoyance', _power = SabrinaFuture), 
-    Hero(name='Will', ability_name='Chunnibyou', _power = WillSac),
+    Hero(name="Brock", ability_name="Brock Solid", _power=BrockShieldPower, showdown_avatar = "brock"),
+    Hero(name="Blaine", ability_name="PUSH THE BUTTON", _power=BlaineButton, showdown_avatar = "blaine"),
+    Hero(name="Blue", ability_name = "Smell 'ya Later", _power=BlueSmell, showdown_avatar = "blue"),
+    Hero(name="Bruno", ability_name="Thicc Bod", _power=BrunoBod, showdown_avatar = "bruno"),
+    Hero(name="Erika", ability_name="Herb Master", _power=ErikaGarden, showdown_avatar = 'erika'),
+    Hero(name="Giovanni", ability_name="Crypto Dividends", _power=GiovanniGains, showdown_avatar = 'giovanni'),
+    Hero(name='Green', ability_name='Mineral Collector', _power = GreensRocks, showdown_avatar = "leaf-gen3"),
+    Hero(name='Janine', ability_name='Ninjutsu Art of Substitution', _power = KogaNinja, showdown_avatar ="koga"),
+    Hero(name='Koga', ability_name='Ninjutsu Art of Summoning', _power = JanineJutsu, showdown_avatar = "janine"),
+    Hero(name='Lance', ability_name='"Dragon" Master', _power = LanceFetish, showdown_avatar = "lance"),
+    Hero(name='Lt. Surge', ability_name='Gorilla Warfare', _power = SurgeGorilla, showdown_avatar = 'ltsurge'),
+    Hero(name='Misty', ability_name='Trust Fund', _power = MistyTrustFund, showdown_avatar = "misty"),
+    Hero(name='Red', ability_name='Plot Armor', _power = RedCheater, showdown_avatar = 'red'),
+    Hero(name='Jessie and James', ability_name='Blast Off!!', _power = BlastOff, showdown_avatar = 'teamrocket​'),
+    Hero(name='Sabrina', ability_name='Clairvoyance', _power = SabrinaFuture, showdown_avatar = 'sabrina'), 
+    Hero(name='Will', ability_name='Chunnibyou', _power = WillSac, showdown_avatar = 'will'),
 ]
 
 PRISMATIC_HEROES = [x for x in NORMAL_HEROES]
